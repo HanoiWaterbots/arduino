c6f7f57ca0d256953d2fdf31236e375a08496c55
Peiyu Liao <u3512485@connect.hku.hk>
2018-06-19 19:24:58 +0800

    Update motor minimum speed

--- Motor/LoRa_Motor/LoRa_Motor.ino
+++ Motor/LoRa_Motor/LoRa_Motor.ino
@@ -90,8 +90,8 @@ void setup() {
   ESC1.attach(2);  
   ESC2.attach(3);
 
-  ESC1.writeMicroseconds(1550);
-  ESC2.writeMicroseconds(1550);
+  ESC1.writeMicroseconds(1600);
+  ESC2.writeMicroseconds(1600);
   delay(2000);
 
   Serial.begin(115200);
@@ -143,4 +143,3 @@ void loop() {
     LoRaReceiver.flush();
   }
 }
-
